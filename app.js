caa919c0204cc8a93f4f500a2efaa5293118c250
Kyle <83605851+kylewz@users.noreply.github.com>
2024-04-09 16:28:33 -0600

    Update packages, update app.js
    Updates express version, adds code for basic

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+'use strict';
+
 // Requirements
 const express = require('express');
-const app = express();
-const { projects } = require('./project-data.json');
 const path = require('path');
+const { projects } = require('./project-data.json');
+
+const app = express();
+
+app.listen(3000, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('listening on port 3000');
+});
