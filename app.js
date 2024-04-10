097439a99b79322562846198d5e67f7f1e3720ac
kylewz <kyle.z@hey.com>
2024-04-10 12:06:52 -0600

    Some practice
    Update personal info in pug templates, adds some code for practice in app.js

--- app.js
+++ app.js
@@ -4,8 +4,15 @@
 const express = require('express');
 const path = require('path');
 const { projects } = require('./project-data.json');
-
+const pug = require('pug');
 const app = express();
+
+// Set view engine to Pug
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(3000, (err) => {
   if (err) {
